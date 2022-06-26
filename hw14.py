3fd98e49c45a85b926d1bb87672f4d10666f23cf
Amantur <swinnytodd225@gmail.com>
2022-06-26 23:17:11 +0300

    homeW

---
+++ hw14.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import pickle
+
+
+
+class Employee:
+    def __init__(self, name):
+        self.name = name
+
+
+Mary = Employee('Mary')
+print(Mary)
+
+with open('hw14.pickle', 'wb') as f:
+    pickle.dump(Mary, f)
+
+with open('hw14.pickle', 'rb') as f:
+    data_emp = pickle.load(f)
+
+print(data_emp.name)
